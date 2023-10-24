beb0a08fb0f742927be56c72acb510e077a06afa
SvitRia <129522432+SvitRia@users.noreply.github.com>
2023-10-24 16:56:31 +0200

    refactoring resdy

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,57 +1,94 @@
-import { Component } from 'react';
+import { useState,  } from 'react';
 import { Feedbackoptions } from './FeedbackOptions/FeedbackOptions';
 import { Statistics } from './Statistics/Statistics';
 
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
 
-  onLeavFeedback = (option) => {
-    this.setState(prevState => {
-      return {
-        [option]: prevState[option] + 1,
-      };
-    });
-  };
+export const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
   
-  onTotalFeedback = () => {
-    const { good, neutral, bad } = this.state;
-    return (
-      good + neutral + bad
-    )
-  }
-  
-  onCheckPositivFeedback = () => {
-    const { good, neutral, bad } = this.state;
-    const total = good + neutral + bad;
+
+  const onaddFeedback = ((key) => {
+    switch (key) {
+      case 'good': 
+        setGood(prevState => prevState + 1)
+        break;
+      case 'neutral':
+        setNeutral(prevState => prevState + 1)
+        break;
+      case 'bad': 
+        setBad(prevState => prevState + 1)
+        break;
+       
+    }
+  });
+ 
+  const TotalFeedback = 
+      good  + neutral + bad
+   ;
    
-    return (
-      Math.round(good / total * 100));
+  const CheckPositivFeedback = 
+      Math.round(good / TotalFeedback * 100);
+ 
+
+  return (
+    <div>
+      <Feedbackoptions options={['good', 'neutral', 'bad']} onFeedback={onaddFeedback} />
+      <Statistics
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        total={TotalFeedback}
+        percent={CheckPositivFeedback}
+      />
+    </div>
+  );
+}
+
+//   onLeavFeedback = (option) => {
+//     this.setState(prevState => {
+//       return {
+//         [option]: prevState[option] + 1,
+//       };
+//     });
+//   };
+  
+//   onTotalFeedback = () => {
+//     const { good, neutral, bad } = this.state;
+//     return (
+//       good + neutral + bad
+//     )
+//   }
+  
+//   onCheckPositivFeedback = () => {
+//     const { good, neutral, bad } = this.state;
+//     const total = good + neutral + bad;
+   
+//     return (
+//       Math.round(good / total * 100));
      
-  }
+//   }
  
 
-  render() {
-    const { good, neutral, bad } = this.state;
-    const total = this.onTotalFeedback();
-    const persent = this.onCheckPositivFeedback();
+//   render() {
+//     const { good, neutral, bad } = this.state;
+//     const total = this.onTotalFeedback();
+//     const persent = this.onCheckPositivFeedback();
 
  
-      return (
-        <div>
-          <Feedbackoptions  options={Object.keys(this.state)} onFeedback={this.onLeavFeedback} />
-          <Statistics
-            good={good}
-            neutral={neutral}
-            bad={bad}
-            total={total}
-            percent={persent}
-          />
-        </div>
-      );
-    };
-  };
+//       return (
+//         <div>
+//           <Feedbackoptions  options={Object.keys(this.state)} onFeedback={this.onLeavFeedback} />
+//           <Statistics
+//             good={good}
+//             neutral={neutral}
+//             bad={bad}
+//             total={total}
+//             percent={persent}
+//           />
+//         </div>
+//       );
+//     };
+//   };
 
